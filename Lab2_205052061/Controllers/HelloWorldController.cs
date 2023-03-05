505d1c19a2a1eed985905ba018e006444c99f869
dungv798 <dungv798@gmail.com>
2023-03-05 15:40:47 +0700

    first commit

---
+++ Lab2_205052061/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //   return "This is my default action...";
+        //}
+        //
+        // GET: /HelloWorld/Welcome/
+        //public string Welcome(String name, int numTimes =1)
+        //{
+        //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+
+    }
+}
